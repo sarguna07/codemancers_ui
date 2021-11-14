866909e92ef584ed3c53ed032104d32c066a0053
sarguna <sarguna1744@gmail.com>
2021-11-14 23:05:59 +0530

    Sarguna: updated answer page

--- src/views/Solution.js
+++ src/views/Solution.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { PageNotFound } from '.';
 import { fetchQuiz, SumbitQuiz } from "../api/solution";
 import Result from "../views/Result"
+import { Button, Card, Radio, Space } from 'antd';
 class Solution extends Component {
     constructor(props) {
         super(props);
@@ -16,11 +17,10 @@ class Solution extends Component {
         };
     }
 
-    handleChange = (event, index, value) => {
-        let ans = this.state.correct_options;
-        ans[index]['id'] = event.target.id
-        ans[index]['answer'] = value
-        this.setState({ correct_options: ans });
+    handleChange = (event, index) => {
+        let { question_data } = this.state;
+        question_data[index]['answer'] = event.target.value
+        this.setState({ question_data });
     };
 
     componentDidMount() {
@@ -35,9 +35,9 @@ class Solution extends Component {
 
     submitAnswer = () => {
         const {
-            correct_options
+            question_data
         } = this.state;
-        SumbitQuiz({ quiz: correct_options })
+        SumbitQuiz({ quiz: question_data })
             .then((data) => {
                 this.setState({
                     result: data.data,
@@ -49,88 +49,47 @@ class Solution extends Component {
             });
 
     }
+
     render() {
         const { question_data, result, question_list } = this.state;
         const { handleChange, submitAnswer } = this;
 
         return (
-            <div>
-                {question_data.length === 0 && <PageNotFound />}
-                {question_data.length !== 0 && question_list &&
-                    (<form
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            submitAnswer();
-                        }}
-                    >
+            <React.Fragment>
+                <header className="header" style={{ backgroundColor: "#0000A5" }}>
+                    <h2 style={{ color: "white" }}>WELCOME TO QUIZ COMPETITION</h2>
+                </header>
+                <div className="my-root">
+                    <div className="root-container">
+                        <div>
 
-                        <div style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            width: "70vw",
-                            margin: "auto",
-                            justifyContent: "center",
-                        }}>
-                            {
-                                question_data.map((q, i) => {
-                                    return (
-                                        <div key={i}>
-                                            <p style={{ textAlign: "center" }}><b>{i + 1}.{q.question}</b></p>
-                                            <div style={{
-                                                display: "flex",
-                                                justifyContent: "space-evenly"
-                                            }}>
-                                                <div style={{ display: "flex", alignItems: "center" }}>
-                                                    <input
-                                                        type="checkbox"
-                                                        id={q.id}
-                                                        onChange={(e) => handleChange(e, i, q.option_one)} />
-                                                    <span style={{ paddingLeft: "10px" }}>{q.option_one}</span>
-                                                </div>
-                                                <div style={{ display: "flex", alignItems: "center" }}>
-                                                    <input type="checkbox"
-                                                        id={q.id}
-                                                        onChange={(e) => handleChange(e, i, q.option_two)} />
-                                                    <span style={{ paddingLeft: "10px" }}>{q.option_two}</span>
-                                                </div>
-                                                <div style={{ display: "flex", alignItems: "center" }}>
-                                                    <input type="checkbox"
-                                                        id={q.id}
-                                                        onChange={(e) => handleChange(e, i, q.option_three)} />
-                                                    <span style={{ paddingLeft: "10px" }}>{q.option_three}</span>
-                                                </div>
-                                                <div style={{ display: "flex", alignItems: "center" }}>
-                                                    <input type="checkbox"
-                                                        id={q.id}
-                                                        onChange={(e) => handleChange(e, i, q.option_four)} />
-                                                    <span style={{ paddingLeft: "10px" }}>{q.option_four}</span>
-                                                </div>
+                            {question_data.length === 0 && <PageNotFound />}
 
-                                            </div>
-                                        </div>
-                                    )
-                                })
+                            {question_list && question_data?.map((question, i) => {
+                                return (
+                                    <Card title={question.question} style={{ marginTop: "10px" }} bordered={true} style={{ width: 530, margin: "10px" }}>
+                                        <Radio.Group value={question.answer} onChange={(e) => handleChange(e, i)} >
+                                            <Space direction="vertical">
+                                                <Radio value={question.option_one}>{question.option_one}</Radio>
+                                                <Radio value={question.option_two}>{question.option_two}</Radio>
+                                                <Radio value={question.option_three}>{question.option_three}</Radio>
+                                                <Radio value={question.option_four}>{question.option_four}</Radio>
+                                            </Space>
+                                        </Radio.Group>
+                                    </Card>
+                                )
                             }
-
-                            <button
-                                style={{
-                                    width: "20%",
-                                    textAlign: "center",
-                                    alignSelf: "center",
-                                    marginTop: "30px",
-                                    color: "white",
-                                    background: "#2860cc",
-                                    padding: "8px 0px",
-                                    border: "none"
-                                }}>
-                                submit
-                            </button>
-
-                        </div >
-                    </form>
-                    )}
-                {question_data.length !== 0 && !question_list && <Result result={result} />}
-            </div>
+                            )}
+                            {question_data.length > 0 && question_list && <Button type="primary" htmlType="submit"
+                                onClick={submitAnswer}
+                            >
+                                Submit
+                            </Button>}
+                            {question_data.length !== 0 && !question_list && <Result result={result} />}
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
